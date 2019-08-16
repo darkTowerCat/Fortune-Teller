c23d34bb0e28ec9206bcf4faf4d7226753222ffe
darkTowerCat <38337802+darkTowerCat@users.noreply.github.com>
2019-08-16 13:54:26 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Fortune Teller
+Copyright (c) 2019 Terry Tower
+
+This program takes a number from the user, mods if with the number or responces, and then outputs an inspirational saying. 
+
+This will be able to help anyone having a bad day, and needs a little ray of sunshine to hang on.
+
+## Build and Run
+
+Use `cargo build` to start the program. Then enter a number when prompted.
+
+## License
+
+This program is licensed under the "MIT License".  Please
+see the file `LICENSE` in the source distribution of this
+software for license terms.
+
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+//Copyright © 2019 Terry Tower
+
+use std::io;
+//based on code from https://stackoverflow.com/questions/30355185/how-to-read-an-integer-input-from-the-user-in-rust-1-0
+
+fn main() {
+	let max_answer = 3;
+	let mut input_text = String::new();
+
+	println!("Thank you for visiting the fortune teller:\nPlease enter your lucky number");
+
+	io::stdin()
+		.read_line(&mut input_text)
+		.expect("failed to read from stdin");
+
+	let trimmed = input_text.trim();
+	match trimmed.parse::<u32>() {
+		Ok(number) => responce(number, max_answer),
+			Err(..) => println!("{} is was not an integer", trimmed),
+	};
+}
+fn responce(number:u32, max_answer:u32) {
+	match number % max_answer {
+		0 => println!("The road is rocky now, but will be worth it."),
+		  1 => println!("Keep going you are almost there."),
+		  2 => println!("Your future is bright."),
+		  3 => println!("No matter how bad today is, tomorrow will come.")
+		  _ => println!("Focus on the light at the end of the tunnel."),
+	}
+}
